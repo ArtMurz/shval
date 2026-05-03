c35195afc27037724b9a4c26eac3e81937e37ec8
ArtMurz <peoplewiner@mail.ru>
2026-05-03 20:02:45 +0700

    Update app.22

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, redirect
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     ip = request.headers.get("X-Forwarded-For", request.remote_addr)
+    user_agent = request.headers.get("User-Agent")
     print("IP:", ip)
 
+    with open("clicks.log", "a", encoding="utf-8") as f:
+        f.write(f"Время - {datetime.now()} | IP - [{user_ip} | Еще что-то {user_agent}\n")
     return redirect("https://www.deepseek.com/en/")  # ← сюда меняешь ссылку
 
 if __name__ == "__main__":
