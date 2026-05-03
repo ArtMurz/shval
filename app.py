6d203ef4b0fd476e74543fd6312651b6c31ccf40
ArtMurz <peoplewiner@mail.ru>
2026-05-03 19:58:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, request, redirect
-
+from datetime import datetime
+from flask import Flask, redirect, request
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    ip = request.headers.get("X-Forwarded-For", request.remote_addr)
-    print("IP:", ip)
+TARGET = "https://cloud.mail.ru/public/C3Nj/"
 
-    return redirect("https://www.deepseek.com/en/")  # ← сюда меняешь ссылку
+@app.route("/")
+def go():
+    # тут ты ловишь факт перехода
+    user_ip = request.remote_addr
+    user_agent = request.headers.get("User-Agent")
+
+    print("Переход по ссылке!")
+    print("IP:", user_ip)
+    print("UA:", user_agent)
+
+    # редирект на нужную страницу
+    with open("clicks.log", "a", encoding="utf-8") as f:
+        f.write(f"Время - {datetime.now()} | IP - [{user_ip} | Еще что-то {user_agent}\n")
+    return redirect("https://cloud.mail.ru/public/C3Nj/8JYTes8G9")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
